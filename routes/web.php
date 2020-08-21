ba697be3b0e1a25e2836eba6dbd37db1b3d621d1
awwalkhalqin <awwala1012@gmail.com>
2020-08-21 13:48:53 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('index');
+    return view('form');
 });
 
+Route::get('/forms', 'formController@index');
+Route::post('/proses', 'formController@proses');
